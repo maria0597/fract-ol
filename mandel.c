27a41d64f7188dabaffc5202e89a0ba7889ec837
maria ardolino <98991538+maria0597@users.noreply.github.com>
2022-09-25 15:00:46 +0200

    Update mandel.c

--- mandel.c
+++ mandel.c
@@ -1,3 +1,19 @@
+/* 
+
+In complex dynamics, the object of central interest in the dynamical plane is the Julia set. 
+For the family Pc, there is an open neighborhood of infinitum in the Reinmann sphere consisting 
+of points whose orbits tend to infinitum. The set of all points whose orbits tend to infinitum
+is called the basin of infinitum. Then the Julia set, deboted by J(Pc) is the boundary of the basin.
+Julia set is the chaotic set.
+
+At each c-value on the boundary, Pc has a neutral cycle, a periodic point z of period n
+for which (Pc ^n)'(z) = exp⁡(2πiθ). As c windsonce around the boundary of this hyperbolic component
+θ winds once around the unit circle. As a consequence, there is a dense set of such c's for which
+θ is rational. In this case, the netural cycle lies in the Julia set but there are still region
+in which all points tend to the neutral cycle. These type of periodic points are called parabolic points.ù
+
+
+*/
 typedef struct s_param{
 	void			*mlx_ptr;
 	void			*win_ptr;
